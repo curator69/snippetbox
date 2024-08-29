0b6ab2620ae34fa9284df24d5fc2a4ce14b27592
Rushikesh <rushikeshkhillari10@gmail.com>
2024-08-30 00:16:53 +0530

    refactored code for form handling and error handling

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"net/url"
 	"path/filepath"
 	"text/template"
 	"time"
 
+	"curator.dev/snippetbox/pkg/forms"
 	"curator.dev/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
